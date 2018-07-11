8ccb375108db80b799b9bd1d153690486a02ce4e
Eliott hauteclair <me@eliott.be>
2018-07-11 10:18:43 +0200

    Fix Syncing problems

--- src/CryptoNoteCore/SwappedMap.h
+++ src/CryptoNoteCore/SwappedMap.h
@@ -10,7 +10,11 @@
 #include <cstdint>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
+
+#ifdef __APPLE__
+    #include <iostream>
+#endif
+
 #include <list>
 #include <unordered_map>
 #include <string>
